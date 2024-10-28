9d577e8e89260badea9aebc96f48c63e82cfeaac
venugopal reddy <nimmanapallivenu@gmail.com>
2024-10-28 20:51:10 +0530

    Print all permutations of given input

---
+++ src/programs/PermutationsString.java
@@ -0,0 +1,24 @@
+package programs;
+
+public class PermutationsString {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Print all permutations of given input");
+
+        String input = "abc";
+        permute(input, "");
+    }
+
+    static void permute(String str, String pre) {
+        if (str.length() == 0) {
+            System.out.println(pre);
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                String rem = str.substring(0, i) + str.substring(i + 1);
+                permute(rem, pre + str.charAt(i));
+
+            }
+        }
+    }
+}
