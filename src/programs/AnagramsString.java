424d88c5a7c6b80bcaa54d4c074df08ecd08ac3f
venugopal reddy <nimmanapallivenu@gmail.com>
2024-10-29 09:07:55 +0530

    Find two String are Anagrams are not

--- src/programs/AnagramsString.java
+++ src/programs/AnagramsString.java
@@ -11,8 +11,30 @@ public class AnagramsString {
         System.out.println("Find two String are Anagrams are not ?");
         String str1 = "listen";
         String str2 = "silent";
-        System.out.println(str1 + "  and  " + str2 + " are Anagrams : " + isAnagram(str1, str2));
+        //System.out.println(str1 + "  and  " + str2 + " are Anagrams : " + isAnagram(str1, str2));
+        System.out.println(str1 + "  and  " + str2 + " are Anagrams : " + isAnagramStrings(str1, str2));
 
+    }
+
+    static boolean isAnagramStrings(String str1, String str2) {
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        if (str1.length() != str2.length()) return false;
+        int[] charCount = new int[26];
+        for (int i = 0; i < str1.length(); i++) {
+            char c1 = str1.charAt(i);
+            char c2 = str2.charAt(i);
+            charCount[c1 - 'a']++;
+            charCount[c2 - 'a']--;
+        }
+
+        for (int ch : charCount) {
+            if (ch != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     static boolean isAnagram(String str1, String str2) {
---
+++ src/programs/arrays/SortArrayElements.java
@@ -0,0 +1,19 @@
+package programs.arrays;
+
+import java.util.Arrays;
+
+public class SortArrayElements {
+
+    public static void main(String[] args) {
+        System.out.println("Sort Array Element without In build functions");
+        int[] arr ={9,2,7,3,6,1};
+        System.out.println("Before Sort Elements");
+        System.out.println(Arrays.toString(arr));
+        sortArray(arr);
+    }
+
+    // selection Sorts
+    private static void sortArray(int[] arr) {
+
+    }
+}
