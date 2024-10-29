d4a06bd078ddea7a0264b357245c3080ccc100e4
venugopal reddy <nimmanapallivenu@gmail.com>
2024-10-29 18:34:37 +0530

    Move all zeros to end of the array

--- src/arrays/MoveAllZeros.java
+++ src/arrays/MoveAllZeros.java
@@ -1,2 +1,32 @@
-package arrays;public class MoveAllZeros {
+package arrays;
+
+import java.util.Arrays;
+
+/*
+Move all zeros to end of the array
+[1, 2, 0, 4, 3, 0, 5, 0]
+[1, 2, 4, 3, 5, 0, 0, 0]
+ */
+public class MoveAllZeros {
+
+    public static void main(String[] args) {
+        System.out.println("Move all zeros to end of the array");
+        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
+        System.out.println(Arrays.toString(arr));
+        int[] result = moveZeros(arr);
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] moveZeros(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[count++] = arr[i];
+            }
+        }
+        while (count < arr.length - 1) {
+            arr[count++] = 0;
+        }
+        return arr;
+    }
 }
